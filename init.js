790733306d418fc8956a41e117fc76e492975b3f
Divya <divvyatripaathi@gmail.com>
2021-02-19 23:23:23 +0100

    completed quiz js

--- init.js
+++ init.js
@@ -1,83 +1,76 @@
-// function for 1st question and eventlistener
-function myfunction(e) {
-  document.getElementById("demo").innerHTML = "Correct answer";
+console.time("time took to complete");
+const questionSet = [
+  {
+    id: "q1",
+    question:
+      "How many of the speeches in Shakespeare's plays are recited by women?",
+    options: ["33%", "17%", "52%"],
+    answer: "17%",
+  },
+  {
+    id: "q2",
+    question:
+      "After Antarctica, what is the most sparsely populated continent?",
+    options: ["Asia", "Europe", "Australia", "Africa"],
+    answer: "Australia",
+  },
+  {
+    id: "q3",
+    question: "What country won the very first FIFA World Cup in 1930?",
+    options: ["Uruguay", "Spain", "Australia", "Brazil"],
+    answer: "Uruguay",
+  },
+  {
+    id: "q4",
+    question: "uhyfhhjfghjgfhgjhgrhjgcjhgcjhgcjhg?",
+    options: ["Asia", "Europe", "Australia", "Africa"],
+    answer: "Australia",
+  },
+];
+
+// starts here
+
+const container = document.getElementById("question-list");
+const newDiv = document.createElement("div");
+container.append(newDiv);
+
+for (let i = 0; i < questionSet.length; i++) {
+  const divContainer = document.createElement("div");
+
+  divContainer.innerHTML = questionSet[i].question;
+
+  const divForBtn = document.createElement("div");
+  const divForAns = document.createElement("div");
+
+  divContainer.className = "divCont";
+
+  divContainer.id = "divCont_" + i;
+  divForBtn.id = "divBtn_id" + i;
+
+  divForBtn.className = "divForBtn_class";
+
+  newDiv.append(divContainer);
+  console.log(divContainer);
+
+  for (let index = 0; index < questionSet[i].options.length; index++) {
+    // const divForBtn = document.createElement("div");
+    const btnContainer = document.createElement("button");
+    btnContainer.innerHTML = questionSet[i].options[index];
+    btnContainer.addEventListener("click", function () {
+      if (questionSet[i].options[index] === questionSet[i].answer) {
+        divForAns.innerHTML = "Correct Answer";
+      } else {
+        divForAns.innerHTML = "Wrong Answer";
+      }
+    });
+
+    btnContainer.className = "btnClass";
+    btnContainer.id = "btnid_" + index;
+
+    divContainer.append(btnContainer);
+    divForBtn.append(btnContainer);
+    newDiv.append(divForBtn);
+    newDiv.append(divForAns);
+  }
 }
-
-function myfunc1(e) {
-  let y = e.target;
-  document.getElementById("demo").innerHTML = "Wrong answer";
-}
-
-document.getElementById("btn1").addEventListener("click", myfunc1);
-document.getElementById("mybtn").addEventListener("click", myfunction);
-document.getElementById("btn3").addEventListener("click", myfunc1);
-document.getElementById("btn4").addEventListener("click", myfunc1);
-
-// function for 2nd question and eventlistener
-
-function myfunction2(e) {
-  let z = e.target;
-  document.getElementById("demo2").innerHTML = "Correct answer";
-}
-
-function myfunc2(e) {
-  let z1 = e.target;
-  document.getElementById("demo2").innerHTML = "Wrong answer";
-}
-
-document.getElementById("hag2").addEventListener("click", myfunc2);
-document.getElementById("oct2").addEventListener("click", myfunction2);
-document.getElementById("ear2").addEventListener("click", myfunc2);
-document.getElementById("lee2").addEventListener("click", myfunc2);
-
-// function for 3rd question and eventlistener
-
-function myfunction3(e) {
-  let z = e.target;
-  document.getElementById("demo3").innerHTML = "Correct answer";
-}
-
-function myfunc3(e) {
-  let z1 = e.target;
-  document.getElementById("demo3").innerHTML = "Wrong answer";
-}
-
-document.getElementById("hag3").addEventListener("click", myfunc3);
-document.getElementById("oct3").addEventListener("click", myfunction3);
-document.getElementById("ear3").addEventListener("click", myfunc3);
-document.getElementById("lee3").addEventListener("click", myfunc3);
-
-// function for 4th question and eventlistener
-
-function myfunction4(e) {
-  let z = e.target;
-  document.getElementById("demo4").innerHTML = "Correct answer";
-}
-
-function myfunc4(e) {
-  let z1 = e.target;
-  console.log(z1);
-  document.getElementById("demo4").innerHTML = "Wrong answer";
-}
-
-document.getElementById("hag4").addEventListener("click", myfunc4);
-document.getElementById("oct4").addEventListener("click", myfunction4);
-document.getElementById("ear4").addEventListener("click", myfunc4);
-document.getElementById("lee4").addEventListener("click", myfunc4);
-
-// function for 5th question and eventlistener
-
-function myfunction5(e) {
-  let z = e.target;
-  document.getElementById("demo5").innerHTML = "Correct answer";
-}
-
-function myfunc5(e) {
-  let z1 = e.target;
-  document.getElementById("demo5").innerHTML = "Wrong answer";
-}
-
-document.getElementById("hag5").addEventListener("click", myfunc5);
-document.getElementById("oct5").addEventListener("click", myfunction5);
-document.getElementById("ear5").addEventListener("click", myfunc5);
-document.getElementById("lee5").addEventListener("click", myfunc5);
+console.timeEnd("time took to complete");
